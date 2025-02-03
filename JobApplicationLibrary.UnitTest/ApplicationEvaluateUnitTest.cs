5e4d5ab1f8bf37015bfd27f68c6c75fe48195a67
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-02-03 16:47:57 +0100

    Add new project structure and unit tests for evaluation
    New Unit test is created to test functionality of ApplicationEvaluator's Evaluate Method. Cheched whether the applicant is UnderAge or not

---
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using JobApplicationLibrary.Models;
+using NUnit.Framework;
+
+namespace JobApplicationLibrary.UnitTest
+{
+    public class ApplicationEvaluateUnitTest
+    {
+        // There are naming rules for UnitTest
+        // UnitOfWork_Condition_ExpectedResult
+        // Condition_Result
+        // FunctionUnderTest_ExpectedResult_UnderCondition
+        [Test]
+        public void Application_TransferredToAutoRejected_WithUnderAge()
+        {
+            // Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 17
+                }
+            };
+            // Action
+
+            var appResult = evaluator.Evaluate(form);
+            // Assert
+
+            Assert.That(appResult, Is.EqualTo(ApplicationResult.AutoRejected));
+
+        }
+    }
+}
